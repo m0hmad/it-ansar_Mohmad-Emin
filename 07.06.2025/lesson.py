1689fc66f474b7088e062f15f70dae03fe9949f7
m0hmad <m0hmad.kll@gmail.com>
2025-06-07 23:52:06 +0300

    Update lesson.py

--- 07.06.2025/lesson.py
+++ 07.06.2025/lesson.py
@@ -7,3 +7,23 @@ word = input("Введите слово: ")
 for char in word:
   print(char)
 
+# 03
+sum = 0
+for i in range(100):
+  sum += i
+print(sum)
+
+# 04
+word = input("Введите слово: ")
+count = 0
+for char in word:
+  if char in "AaEeIiOoUu":
+    count += 1
+print("Количество гласных:", count)
+
+# 05
+word = input("Введите слово:")
+for i in range (0, len(word), 2):
+  print(word[i])
+  
+

7257b3799e075379f021e22eb9b3786cb069eb0d
m0hmad <m0hmad.kll@gmail.com>
2025-06-08 00:47:17 +0300

    Update lesson.py

--- 07.06.2025/lesson.py
+++ 07.06.2025/lesson.py
@@ -1,4 +1,4 @@
-# 01
+# 01 
 for i in range(1, 11):
   print(i)
 
@@ -25,5 +25,23 @@ print("Количество гласных:", count)
 word = input("Введите слово:")
 for i in range (0, len(word), 2):
   print(word[i])
-  
 
+# 06
+count = 0
+for i in range(1, 51):
+  if i % 7 != 0:
+    count += i:
+print("Сумма чисел от 1 до 50, исключая кратные 7:", count)
+
+# 07
+num = int(input("Введите число: "))
+fact = 1
+for i in range(1, num + 1):
+  fact *= i
+print("Факториал числа", num, "равен:", fact)
+
+# 08
+sum_squares = 0
+for i in range(1, 11):
+  sum_squares += i ** 2
+print(f"Сумма квадратов чисел от {1} до {10} равен: {sum_squares}")

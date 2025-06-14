9240b328705361c1dc689f284d1a1453301d402e
m0hmad <m0hmad.kll@gmail.com>
2025-06-15 00:58:19 +0300

    Delete 02.06.2025/lesson02.py

--- 02.06.2025/lesson02.py
+++
@@ -1,5 +0,0 @@
-total = int(input("Введите кол-во участников: "))
-participants = list()
-for _ in range(total):
-  participants.append(input("Введите имя участника: "))
-print("Все участники зарегистрированы")

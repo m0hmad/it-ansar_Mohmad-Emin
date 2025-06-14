6957586e298d2787143219cf5b8df3e9354aaa09
m0hmad <m0hmad.kll@gmail.com>
2025-06-15 00:59:31 +0300

    Delete 02.06.2025/lesson09.py

--- 02.06.2025/lesson09.py
+++
@@ -1,9 +0,0 @@
-count = 0
-while (passwd := input("Введите пароль: ")) != "secret":
-  print("Пароль неверный, попробуйте еще раз")
-  count += 1
-  if count == 5:
-    print("Попытки закончились")
-    break
-else:
-  print("Доступ разрешен")
